5b3014e54876a97257b2f5eda2416fba33dc7c36
Marie Pelletier <marie.pelletier@gmail.com>
2016-07-19 13:58:04 -0400

    initial commit

---
+++ middleware.js
@@ -0,0 +1,10 @@
+module.exports = {
+  requireAuthentification: function(req,res,next){
+    console.log('private route hit!');
+    next();
+  },
+  logger: function(req,res,next){
+    console.log("Request: "+req.method+" "+req.originalUrl+" "+new Date().toString());
+    next();
+  }
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+//create express app:
+var app = express();
+//create route (get requests)
+
+var middleware = require('./middleware');
+
+app.use(middleware.logger);
+
+app.get('/about',middleware.requireAuthentification, function(req,res){
+    res.send('About us');
+});
+
+app.use(express.static(__dirname + "/public"));
+
+//listen to port
+app.listen(3000);
